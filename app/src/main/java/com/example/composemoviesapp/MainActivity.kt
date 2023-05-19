37ef786643760bdd5f180e538fb29c09df48a11a
Ramiz Raja <informramiz@gmail.com>
2023-05-20 01:06:16 +0500

    feat: Build basic structure of movies list

--- app/src/main/java/com/example/composemoviesapp/MainActivity.kt
+++ app/src/main/java/com/example/composemoviesapp/MainActivity.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,7 +25,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composemoviesapp.ui.theme.ComposeMoviesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,12 +67,44 @@ private fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 private fun MainContent() {
-    Text(
-        text = "Hello World",
-        color = MaterialTheme.colorScheme.onBackground
+    val movies = listOf(
+        "Harry Potter",
+        "Justice League",
+        "300",
+        "Avengers: Endgame",
+        "Spiderman noway home",
+        "Avengers: Infinity War",
+        "The Batman"
     )
+    MoviesList(movies = movies)
 }
 
+@Composable
+private fun MoviesList(movies: List<String>) {
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp)
+    ) {
+        items(movies) {
+            MovieRow(movie = it)
+        }
+    }
+}
+
+@Composable
+private fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(120.dp)
+        ,
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Text(text = movie)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
