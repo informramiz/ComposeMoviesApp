0146dfa2a604787207cee114403dc801a4c74a74
Ramiz Raja <informramiz@gmail.com>
2023-05-22 02:17:07 +0500

    refactor: Minor UI padding adjustments

--- app/src/main/java/com/example/composemoviesapp/screens/moviedetails/MovieDetailsScreen.kt
+++ app/src/main/java/com/example/composemoviesapp/screens/moviedetails/MovieDetailsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -65,9 +66,13 @@ fun MovieDetailsScreen(movieId: String, movieRepository: MovieRepository, onNavi
         Box(
             modifier = Modifier.padding(contentPadding),
         ) {
-            Column {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 MovieRow(modifier = Modifier.padding(12.dp), movie = movie, onItemClick = {})
                 Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Movie Posters", style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier = Modifier.height(20.dp))
                 HorizontalScrollableImagesView(images = movie.images)
             }
         }
