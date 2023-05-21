a5d2ea36f95b4fb022aa3670c966dc50e627b741
Ramiz Raja <informramiz@gmail.com>
2023-05-22 00:57:08 +0500

    feat: Make card expandable with more data

--- app/src/main/java/com/example/composemoviesapp/ui/widgets/Widgets.kt
+++ app/src/main/java/com/example/composemoviesapp/ui/widgets/Widgets.kt
@@ -1,5 +1,6 @@
 package com.example.composemoviesapp.ui.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,17 +9,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,11 +46,15 @@ import com.example.composemoviesapp.data.model.Movie
 
 @Composable
 fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
+    val expandedState = remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(120.dp)
+            .wrapContentHeight()
             .clickable { onItemClick(movie.id) },
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
@@ -67,7 +79,9 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
             )
 
             Column(
-                modifier = Modifier.padding(6.dp)
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
             ) {
                 Text(
                     text = movie.title,
@@ -82,6 +96,44 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
                 Text(
                     text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.bodySmall
+                )
+
+                AnimatedVisibility(visible = expandedState.value) {
+                    Column {
+                        Divider()
+                        Text(
+                            modifier = Modifier.padding(top = 4.dp),
+                            text = "Plot: ${movie.plot}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(top = 4.dp),
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(top = 4.dp),
+                            text = "Rating: ${movie.rating}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
+
+                Icon(
+                    modifier = Modifier
+                        .size(25.dp)
+                        .align(Alignment.End)
+                        .clickable {
+                            expandedState.value = !expandedState.value
+                        },
+                    imageVector = if (expandedState.value) {
+                        Icons.Default.KeyboardArrowUp
+                    } else {
+                        Icons.Default.KeyboardArrowDown
+                    },
+                    contentDescription = "Down arrow"
                 )
             }
         }
